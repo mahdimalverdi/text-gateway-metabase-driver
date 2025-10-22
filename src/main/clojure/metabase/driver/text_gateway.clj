523bbb5ac93f311587e10e333b74b2f6521643a3
Mahdi Malverdi <malverdi@yektanet.ir>
2025-10-22 22:52:13 +0330

    init

--- src/main/clojure/metabase/driver/text_gateway.clj
+++ src/main/clojure/metabase/driver/text_gateway.clj
@@ -6,13 +6,15 @@
     [metabase.driver :as driver]
     [metabase.util.log :as log]))
 
-;; Register driver keyword
+;; visible side-effect so you can see it in Metabase logs on startup
+(log/info "Loading Text Gateway driver namespace")
+
+;; register driver keyword
 (driver/register! :text-gateway)
 
 (defn- trim-slash [^String s]
   (if (and s (.endsWith s "/")) (subs s 0 (dec (count s))) s))
 
-;; Connection props come from manifest; we don't add more here.
 (defmethod driver/connection-properties :text-gateway [_] [])
 
 (defmethod driver/can-connect? :text-gateway [_ {:keys [base_url api_key]}]
@@ -30,60 +32,44 @@
 (defmethod driver/describe-database :text-gateway
   [_ _] {:name "text-gateway" :tables []})
 
-;; --- Execute (new reducible API) -------------------------------------------------
-(try
-  (require 'metabase.query-processor.interface)
-  (catch Throwable _))
+;; Try reducible API if available
+(try (require 'metabase.query-processor.interface) (catch Throwable _))
 
 (when (find-ns 'metabase.query-processor.interface)
   (defmethod driver/execute-reducible-query :text-gateway
-    [driver {:keys [settings]} {{:keys [query]} :native :as _mb-query} _context respond! raise!]
+    [driver {:keys [settings]} {{:keys [query]} :native} _ respond! raise!]
     (try
-      (let [base-url (:base_url settings)
-            api-key  (:api_key settings)
-            endpoint (str (trim-slash base-url) "/run")
-            resp (http/post endpoint
-                            {:headers (cond-> {"Content-Type" "application/json"}
-                                      api-key (assoc "Authorization" (str "Bearer " api_key)))
-                             :body (json/encode {:text (str (or query ""))})
-                             :throw-exceptions true
-                             :as :string})
+      (let [endpoint (str (trim-slash (:base_url settings)) "/run")
+            headers  (cond-> {"Content-Type" "application/json"}
+                       (:api_key settings) (assoc "Authorization" (str "Bearer " (:api_key settings))))
+            resp     (http/post endpoint {:headers headers
+                                          :body (json/encode {:text (str (or query ""))})
+                                          :throw-exceptions true
+                                          :as :string})
             body (json/parse-string (:body resp) keyword)
             rows (vec (get body :rows []))
             cols (->> (if (seq rows) (keys (first rows)) [])
-                      (map (fn [k] {:name (name k)
-                                    :display_name (name k)
-                                    :base_type :type/Text}))
-                      vec)
-            data {:columns cols
-                  :rows (mapv (fn [r] (mapv #(get r (keyword (:name %))) cols)) rows)}]
-        (respond! data))
-      (catch Exception e
-        (raise! e)))))
+                      (map (fn [k] {:name (name k) :display_name (name k) :base_type :type/Text}))
+                      vec)]
+        (respond! {:columns cols
+                   :rows (mapv (fn [r] (mapv #(get r (keyword (:name %))) cols)) rows)}))
+      (catch Exception e (raise! e)))))
 
-;; --- Execute (legacy API fallback) ----------------------------------------------
-;; Some older Metabase versions call this 3-arity variant.
+;; Fallback execute-query for older Metabase
 (defmethod driver/execute-query :text-gateway
-  [driver {{:keys [query]} :native :as _mb-query} _context]
-  (try
-    (let [{:keys [settings]} _context
-          base-url (:base_url settings)
-          api-key  (:api_key settings)
-          endpoint (str (trim-slash base-url) "/run")
-          resp (http/post endpoint
-                          {:headers (cond-> {"Content-Type" "application/json"}
-                                    api-key (assoc "Authorization" (str "Bearer " api_key)))
-                           :body (json/encode {:text (str (or query ""))})
-                           :throw-exceptions true
-                           :as :string})
-          body (json/parse-string (:body resp) keyword)
-          rows (vec (get body :rows []))
-          cols (->> (if (seq rows) (keys (first rows)) [])
-                    (map (fn [k] {:name (name k)
-                                  :display_name (name k)
-                                  :base_type :type/Text}))
-                    vec)]
-      {:columns cols
-       :rows (mapv (fn [r] (mapv #(get r (keyword (:name %))) cols)) rows)})
-    (catch Exception e
-      (throw e))))
+  [driver {{:keys [query]} :native} context]
+  (let [{:keys [settings]} context
+        endpoint (str (trim-slash (:base_url settings)) "/run")
+        headers  (cond-> {"Content-Type" "application/json"}
+                   (:api_key settings) (assoc "Authorization" (str "Bearer " (:api_key settings))))
+        resp     (http/post endpoint {:headers headers
+                                      :body (json/encode {:text (str (or query ""))})
+                                      :throw-exceptions true
+                                      :as :string})
+        body (json/parse-string (:body resp) keyword)
+        rows (vec (get body :rows []))
+        cols (->> (if (seq rows) (keys (first rows)) [])
+                  (map (fn [k] {:name (name k) :display_name (name k) :base_type :type/Text}))
+                  vec)]
+    {:columns cols
+     :rows (mapv (fn [r] (mapv #(get r (keyword (:name %))) cols)) rows)}))
