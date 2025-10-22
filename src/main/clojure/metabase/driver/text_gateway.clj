f051d290e2e450403747b98de173c847e116cc46
Mahdi Malverdi <malverdi@yektanet.ir>
2025-10-22 22:34:37 +0330

    init

--- src/main/clojure/metabase/driver/text_gateway.clj
+++ src/main/clojure/metabase/driver/text_gateway.clj
@@ -4,7 +4,6 @@
     [clj-http.client :as http]
     [clojure.string :as str]
     [metabase.driver :as driver]
-    [metabase.query-processor.interface :as qpi]
     [metabase.util.log :as log]))
 
 ;; Register driver keyword
@@ -13,10 +12,7 @@
 (defn- trim-slash [^String s]
   (if (and s (.endsWith s "/")) (subs s 0 (dec (count s))) s))
 
-(defmethod driver/supports? :text-gateway [_ feature]
-  ;; We support native parameters in the editor; actual query is free-form text.
-  (contains? #{:native-parameters} feature))
-
+;; Connection props come from manifest; we don't add more here.
 (defmethod driver/connection-properties :text-gateway [_] [])
 
 (defmethod driver/can-connect? :text-gateway [_ {:keys [base_url api_key]}]
@@ -34,17 +30,49 @@
 (defmethod driver/describe-database :text-gateway
   [_ _] {:name "text-gateway" :tables []})
 
-;; Reducible query execution; Metabase calls this with the expanded native query string.
-(defmethod driver/execute-reducible-query :text-gateway
-  [driver {:keys [settings] :as _database} {{:keys [query]} :native :as mb-query} context respond! raise!]
+;; --- Execute (new reducible API) -------------------------------------------------
+(try
+  (require 'metabase.query-processor.interface)
+  (catch Throwable _))
+
+(when (find-ns 'metabase.query-processor.interface)
+  (defmethod driver/execute-reducible-query :text-gateway
+    [driver {:keys [settings]} {{:keys [query]} :native :as _mb-query} _context respond! raise!]
+    (try
+      (let [base-url (:base_url settings)
+            api-key  (:api_key settings)
+            endpoint (str (trim-slash base-url) "/run")
+            resp (http/post endpoint
+                            {:headers (cond-> {"Content-Type" "application/json"}
+                                      api-key (assoc "Authorization" (str "Bearer " api_key)))
+                             :body (json/encode {:text (str (or query ""))})
+                             :throw-exceptions true
+                             :as :string})
+            body (json/parse-string (:body resp) keyword)
+            rows (vec (get body :rows []))
+            cols (->> (if (seq rows) (keys (first rows)) [])
+                      (map (fn [k] {:name (name k)
+                                    :display_name (name k)
+                                    :base_type :type/Text}))
+                      vec)
+            data {:columns cols
+                  :rows (mapv (fn [r] (mapv #(get r (keyword (:name %))) cols)) rows)}]
+        (respond! data))
+      (catch Exception e
+        (raise! e)))))
+
+;; --- Execute (legacy API fallback) ----------------------------------------------
+;; Some older Metabase versions call this 3-arity variant.
+(defmethod driver/execute-query :text-gateway
+  [driver {{:keys [query]} :native :as _mb-query} _context]
   (try
-    (let [base-url (:base_url settings)
+    (let [{:keys [settings]} _context
+          base-url (:base_url settings)
           api-key  (:api_key settings)
           endpoint (str (trim-slash base-url) "/run")
-          ;; The body our service expects: {"text":"<query string from editor>"}
           resp (http/post endpoint
                           {:headers (cond-> {"Content-Type" "application/json"}
-                                      api-key (assoc "Authorization" (str "Bearer " api-key)))
+                                    api-key (assoc "Authorization" (str "Bearer " api_key)))
                            :body (json/encode {:text (str (or query ""))})
                            :throw-exceptions true
                            :as :string})
@@ -54,9 +82,8 @@
                     (map (fn [k] {:name (name k)
                                   :display_name (name k)
                                   :base_type :type/Text}))
-                    vec)
-          data {:columns cols
-                :rows (mapv (fn [r] (mapv #(get r (keyword (:name %))) cols)) rows)}]
-      (respond! data))
+                    vec)]
+      {:columns cols
+       :rows (mapv (fn [r] (mapv #(get r (keyword (:name %))) cols)) rows)})
     (catch Exception e
-      (raise! e))))
+      (throw e))))
